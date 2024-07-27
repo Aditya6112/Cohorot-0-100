25217f0f474cfea2a4fa827b647cbfb0c64ba1bb
ADITYA RAJ <raj8017933@gmail.com>
2024-07-28 01:16:18 +0530

    lazy Suspense

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,36 +1,38 @@
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+import { lazy, Suspense } from 'react'
+const Dashboard = lazy(() => import('./components/Dashboard'))
+const Landing = lazy(() => import('./components/Landing'))
 
 function App() {
+  //suspense API
   return (
     <div>
       <BrowserRouter>
-      <Appbar/>
+        <Appbar />
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Suspense fallback="...loading"><Dashboard /></Suspense>} />
+          <Route path="/" element={<Suspense fallback="...loading"><Landing /></Suspense>} />
         </Routes>
       </BrowserRouter>
     </div>
   )
 }
 
-function Appbar(){
+function Appbar() {
   const navigate = useNavigate();
-  return(
+  return (
     <div>
-        <button onClick={() => {
-          // window.location.href = "/";
-          navigate("/")
-        }}>Landing Page</button>
-        <button onClick={() => {
-          // window.location.href = "/dashboard";
-          navigate("/dashboard")
-        }}>Dashboard Page</button>
-        {/* not work beacuse navigate should be in Browser Router */}
-      </div>
+      <button onClick={() => {
+        // window.location.href = "/";
+        navigate("/")
+      }}>Landing Page</button>
+      <button onClick={() => {
+        // window.location.href = "/dashboard";
+        navigate("/dashboard")
+      }}>Dashboard Page</button>
+      {/* not work beacuse navigate should be in Browser Router */}
+    </div>
   )
 }
 
