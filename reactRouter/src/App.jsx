cb88b7ca3a530d95113b2ffb2fb1ee0fd8c5678b
ADITYA RAJ <raj8017933@gmail.com>
2024-07-27 23:14:30 +0530

    Routes

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Dashboard } from './components/Dashboard'
+import { Landing } from './components/Landing'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route Path="/dashboard" element={<Dashboard />} />
+        <Route Path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
