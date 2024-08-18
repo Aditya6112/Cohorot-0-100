2b1914daf98a9979e4415489fb2c88b3b3a8736f
ADITYA RAJ <raj8017933@gmail.com>
2024-08-19 01:20:19 +0530

    Custom hooks

--- Hooks-Week9/src/App.jsx
+++ Hooks-Week9/src/App.jsx
@@ -1,32 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-function App() {
-  const [render, setRender] = useState(true);
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
+function useTodos(n) {
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
-    setInterval(() => {
-      setRender(r => !r);
-    }, 5000)
-  }, []);
+    const value=setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+        .then(res => {
+          setTodos(res.data.todos);
+          setLoading(false)
+        })
+    },n * 1000)
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+        setLoading(false)
+      })
+      //because if we havn't cleane the old clock that had stopped if we don't
+      //do it then there were 2 intervals running in the stack
+      return(
+        clearInterval(value)
+      )
+  }, [n])
+  return { todos, loading };
+}
 
+function App() {
+
+  const { todos, loading } = useTodos(10);
+  if (loading) {
+    return <div>Loading...</div>
+  }
   return (
     <>
-      {render ? <MyComponent /> : <div></div>}
+      {todos.map(todo => <Track todo={todo} />)}
     </>
   )
 }
-function MyComponent() {
-  useEffect(() => {
-    console.error("component mounted")
-    //lifecycle event after 10 sec component unmounted get logged
-    return () => {
-      console.log("component unmounted")
-    };
-  }, []);
 
-  return <>
-    From inside my component
-  </>
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
 }
 
 export default App
