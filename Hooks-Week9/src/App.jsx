78d945c40672d5e3a1a8b3df636fd30a18b0d1d8
ADITYA RAJ <raj8017933@gmail.com>
2024-08-19 02:21:03 +0530

    mouse pointer coordinates

--- Hooks-Week9/src/App.jsx
+++ Hooks-Week9/src/App.jsx
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 
-function useIsOnline(){
-  const [isOnline,setIsOnline] = useState(window.navigator.onLine);
-  useEffect(()=>{
-    window.addEventListener("online",()=>{
-      setIsOnline(true);
-    })
-    window.addEventListener("offline",()=>{
-      setIsOnline(false);
-    })
-  })
-  return isOnline;
-}
+const useMousePointer = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return position;
+};
 
 function App() {
-  const isOnline = useIsOnline(5);
+  const mousePointer = useMousePointer();
+
   return (
     <>
-      {isOnline ? "You are online yay!" : "You are not online"}
+      Your mouse position is {mousePointer.x} {mousePointer.y}
     </>
   )
 }
