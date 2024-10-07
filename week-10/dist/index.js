f7dc65c4ec2192326674266741dd07d871a807af
ADITYA RAJ <raj8017933@gmail.com>
2024-10-07 13:53:42 +0530

    week-10 postgres Sql

---
+++ week-10/dist/index.js
@@ -0,0 +1,53 @@
+"use strict";
+// // write a function to create a users table in your database.
+// import { Client } from 'pg'
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const client = new Client({
+// //   connectionString: "postgresql://postgres:mysecretpassword@localhost/postgres"
+//     connectionString: "postgresql://postgres:aditya@localhost:5432/mynewdb?schema=public"
+// })
+// async function createUsersTable() {
+//     await client.connect()
+//     const result = await client.query(`
+//         CREATE TABLE users (
+//             id SERIAL PRIMARY KEY,
+//             username VARCHAR(50) UNIQUE NOT NULL,
+//             email VARCHAR(255) UNIQUE NOT NULL,
+//             password VARCHAR(255) NOT NULL,
+//             created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+//         );
+//     `)
+//     console.log(result)
+// }
+// createUsersTable();
+const pg_1 = require("pg");
+// Async function to insert data into a table
+function insertData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const client = new pg_1.Client({
+            connectionString: "postgresql://postgres:aditya@localhost:5432/mynewdb?schema=public"
+        });
+        try {
+            yield client.connect(); // Ensure client connection is established
+            const insertQuery = "INSERT INTO users (username, email, password) VALUES ('username2', 'user3@example.com', 'user_password');";
+            const res = yield client.query(insertQuery);
+            console.log('Insertion success:', res); // Output insertion result
+        }
+        catch (err) {
+            console.error('Error during the insertion:', err);
+        }
+        finally {
+            yield client.end(); // Close the client connection
+        }
+    });
+}
+insertData();
