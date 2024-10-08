8af1225888bcbe3d36fb8be96272eff42cf61436
ADITYA RAJ <raj8017933@gmail.com>
2024-10-08 17:35:47 +0530

    Updated into db

--- week-10_Prisma_ORMs/src/index.ts
+++ week-10_Prisma_ORMs/src/index.ts
@@ -1,16 +1,44 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
+
+// async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+//     const res = await prisma.user.create({
+//         data: {
+//             email: username,
+//             password,
+//             firstName,
+//             lastName
+//         }
+//     })
+//     console.log(res);
+// }
+// // insertUser("raj8017933@gmail.com","AdityaRaj","Aditya","Raj");
+// insertUser("amansingh@gmail.com","okjiRaj","Aman","Singh");
+
+//Update
+import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-    const res = await prisma.user.create({
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+    const res = await prisma.user.update({
+        where: { email: username },
         data: {
-            email: username,
-            password,
             firstName,
             lastName
         }
     })
     console.log(res);
 }
-// insertUser("raj8017933@gmail.com","AdityaRaj","Aditya","Raj");
-insertUser("amansingh@gmail.com","okjiRaj","Aman","Singh");
+updateUser('raj8017933@gmail.com', {
+    firstName: "Aditya",
+    lastName: "Singh"
+})
